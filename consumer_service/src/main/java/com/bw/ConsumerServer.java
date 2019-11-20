915e0584558f2361e4ace293737702197ea741b7
oldRay-leilei <57738603+oldRay-leilei@users.noreply.github.com>
2019-11-20 20:44:42 +0800

    导入配置中心的yml配置

--- consumer_service/src/main/java/com/bw/ConsumerServer.java
+++ consumer_service/src/main/java/com/bw/ConsumerServer.java
@@ -1,5 +1,6 @@
 package com.bw;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class ConsumerServer {
     public static void main(String[] args) {
+
         SpringApplication.run(ConsumerServer.class,args);
     }
 }
