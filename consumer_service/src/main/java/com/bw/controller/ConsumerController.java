e363d8109ce5296e185b82bc9b78bdf75c24f2e6
oldRay-leilei <57738603+oldRay-leilei@users.noreply.github.com>
2019-11-21 20:01:26 +0800

    消费方与提供方初步配置

--- consumer_service/src/main/java/com/bw/controller/ConsumerController.java
+++ consumer_service/src/main/java/com/bw/controller/ConsumerController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
-@EnableFeignClients
 @RestController
 @RequestMapping("/consumer")
 public class ConsumerController {
